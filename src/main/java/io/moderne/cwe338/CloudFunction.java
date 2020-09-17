79a9e2cad801e40bb52373a57ab7c92519062539
Jonathan Schneider <jkschneider@gmail.com>
2020-09-17 16:49:22 -0700

    Rewrite 5.1.0

--- src/main/java/io/moderne/cwe338/CloudFunction.java
+++ src/main/java/io/moderne/cwe338/CloudFunction.java
@@ -3,27 +3,29 @@ package io.moderne.cwe338;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
-import org.apache.commons.io.Charsets;
 import org.openrewrite.Refactor;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.tree.J;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 public class CloudFunction implements HttpFunction {
-    private static final JavaParser parser = JavaParser.fromJavaVersion()
+    JavaParser parser = JavaParser.fromJavaVersion()
             .classpath(JavaParser.dependenciesFromClasspath("commons-lang3"))
             .build();
 
-    private static final Refactor refactor = new Refactor()
-            .visit(new CWE338());
-
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
-        J.CompilationUnit fixed = refactor.fixed(parser.reset()
-                .parse(new String(request.getInputStream().readAllBytes(), Charsets.UTF_8))
-                .iterator()
-                .next());
+        Refactor refactor = new Refactor()
+                .visit(new CWE338());
+
+        String input = new String(request.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+
+        List<J.CompilationUnit> parse = parser.reset().parse(input);
+        J.CompilationUnit fixed = refactor.fixed(parse.iterator().next());
 
         assert fixed != null;
-        response.getWriter().write(fixed.printTrimmed());
+        response.getWriter().write(fixed.print());
     }
 }
